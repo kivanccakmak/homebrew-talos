37f8d3489a1aa29dccf50c726ddc413e073e6100
Kıvanç Çakmak <kivanccakmak@Kvancs-MacBook-Air.local>
2026-03-15 01:23:38 +0300

    Update label printer 1.1.8, add Windows talcli builds

--- Casks/talos-label-printer.rb
+++ Casks/talos-label-printer.rb
@@ -1,8 +1,8 @@
 cask "talos-label-printer" do
   version "1.1.8"
-  sha256 "5c150cb493f7179cc8ca4c86db6b895975910d17b50989203e7963c77f496b0c"
+  sha256 "0d031100e8519dbec7a864b41a04224c4f8a399888b1d17564a748ca07214c4e"
 
-  url "https://github.com/kivanccakmak/talos-desktop-tools/releases/download/v#{version}/talos-label-printer-#{version}-arm64.dmg"
+  url "https://github.com/kivanccakmak/talos-desktop-tools/releases/download/v1.1.9/talos-label-printer-#{version}-arm64.dmg"
   name "Talos Label Printer"
   desc "Label printer desktop app for Talos Works"
   homepage "https://talos.works"
