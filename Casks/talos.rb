f036f97800e431a833274523028310f2df14476a
Kıvanç Çakmak <kivanccakmak@gmail.com>
2026-07-11 13:13:35 +0300

    talos 2.2.4

--- Casks/talos.rb
+++ Casks/talos.rb
@@ -1,6 +1,6 @@
 cask "talos" do
-  version "2.2.3"
-  sha256 "b5c516cf85c0ba6961a9eb89c4a0f1345abe27d356267e6078daebb7d16af91c"
+  version "2.2.4"
+  sha256 "6843568b9bfa7a399c5499328d00738012f572a6c378f4693b1d9d0f77d07b31"
 
   url "https://github.com/kivanccakmak/talos-binaries/releases/download/v#{version}/Talos-#{version}-macos-arm64.dmg",
       verified: "github.com/kivanccakmak/talos-binaries/"
